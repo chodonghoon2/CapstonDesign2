07cf86fd44de018987ecc3ea5c5af38c26f42464
chodonghoon2 <112090479+chodonghoon2@users.noreply.github.com>
2022-11-30 01:27:40 +0900

    test
    tq

---
+++ app/src/main/java/com/example/capstonproject/yligin_page.java
@@ -0,0 +1,14 @@
+package com.example.capstonproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class yligin_page extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_yligin_page);
+    }
+}
