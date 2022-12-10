f4fcd3a5dfcd49a04a6a97645687efa4c6c323c2
adonelee <akrso520@naver.com>
2022-12-10 23:24:03 +0900

    Revert "12/10 임시"
    This reverts commit 4e8c70a29c646e5fbb3d53ae82f7a982dfe3ec8f.

--- app/src/main/java/com/example/capstonproject/NoticeObj.java
+++ app/src/main/java/com/example/capstonproject/NoticeObj.java
@@ -7,13 +7,15 @@ import java.time.LocalDate;
 import java.time.LocalDateTime;
 
 public class NoticeObj {
-    String MYID;
-    public NoticeObj(String MYID){
+    String MYID, SENDID;
+    public NoticeObj(String MYID, String SENDID){
         this.MYID = MYID;
+        this.SENDID = SENDID;
     }
 
-    public String sendToMSG(String Msg, String SENDID){
+    public String sendToMSG(String Msg){
         String now;
+        SystemClock.elapsedRealtime();
         try{
             yTask sendTask = new yTask("sendNotice");
             String result = sendTask.execute("&a=1&sendId=" + MYID + "&recvId=" + SENDID + "&message=" + Msg + "&send_time=" + "").get();
@@ -29,33 +31,5 @@ public class NoticeObj {
         return "보내기실패";
     }
 
-    public int count_notices(){
-        try{
-            yTask countTask = new yTask("countNotice");
-            String result = countTask.execute("&a=1&myId=" + MYID).get();
-            Log.e("notice-log", result);
 
-            return Integer.parseInt(result);
-        }catch(Exception e){
-            Log.e("notice-log", e.getMessage());
-        }
-
-        return -1;
-    }
-
-    public String[] call_notice(){
-        try{
-            yTask callTask = new yTask("callNotices");
-            String result = callTask.execute("&a=1&myId=" + MYID).get();
-            Log.e("notice-log", result);
-            // 보낸사람id, 메세지, 보낸시간 묶음으로 받음.
-            String[] notice_list = result.split("/");
-
-            return notice_list;
-        }catch(Exception e){
-            Log.e("notice-log", e.getMessage());
-        }
-
-        return null;
-    }
 }
--- app/src/main/java/com/example/capstonproject/cNotice.java
+++ app/src/main/java/com/example/capstonproject/cNotice.java
@@ -5,29 +5,12 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.Menu;
-import android.widget.ListView;
 
 public class cNotice extends AppCompatActivity {
 
-    ListView notice_listview;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cnotice);
-        SharedPreferences USERINFO = getSharedPreferences("USERINFO", MODE_PRIVATE);
-
-        notice_listview = (ListView) findViewById(R.id.notice_listview);
-        y_notice_adapter adapter = new y_notice_adapter();
-        notice_listview.setAdapter(adapter);
-
-        String user_id = USERINFO.getString("id", "id없음");
-        String[] nList = new NoticeObj(user_id).call_notice();
-
-        for(String noticeItem : nList){
-            String[] noticeInfo = noticeItem.split(",");
-            //send_id, message, send_time
-            adapter.addItem(noticeInfo[1], noticeInfo[0], noticeInfo[2]);
-        }
-
     }
 }
